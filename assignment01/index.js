bdd5a1f190ef5fb65c204708a8466fd965a7fd33
Łukasz Tshipenchko <dev@zxc.sx>
2023-12-25 21:13:40 +0600

    Move sessions into PostgreSQL

--- assignment01/index.js
+++ assignment01/index.js
@@ -4,6 +4,8 @@
 require("dotenv").config();
 const express = require("express");
 const pg = require("pg");
+const session = require("express-session");
+const pgSession = require("connect-pg-simple")(session);
 
 const routes = require("./routes");
 const { attachUser } = require("./auth");
@@ -18,26 +20,29 @@ async function main() {
     app.set("views", __dirname + "/templates");
     app.set("view engine", "ejs");
     app.use(express.urlencoded({ extended: true }));
-
     app.use(require("cookie-parser")());
+
+    const db = new pg.Pool({
+        connectionString:
+            process.env.POSTGRES_DSN || panic("POSTGRES_DSN not set"),
+    });
+
+    /** @type {DI} */
+    const di = { db };
+    await init(di);
     app.use(
         require("express-session")({
             secret:
                 process.env.SESSION_SECRET || panic("SESSION_SECRET not set"),
             resave: false,
             saveUninitialized: false,
+            store: new pgSession({
+                pool: db,
+            }),
         }),
     );
-
-    const db = new pg.Pool({
-        connectionString: process.env.POSTGRES_DSN,
-    });
-
-    /** @type {DI} */
-    const di = { db };
     app.use(attachUser(di));
     routes.register(app, di);
-    await init(di);
 
     const host = process.env.HOST || "0.0.0.0";
     const port = process.env.PORT || 3000;
@@ -56,9 +61,19 @@ async function init(di) {
             username VARCHAR(255) UNIQUE NOT NULL
         )
     `);
+    await di.db.query(`
+        CREATE TABLE IF NOT EXISTS "session"
+        (
+            "sid"    varchar      NOT NULL COLLATE "default",
+            "sess"   json         NOT NULL,
+            "expire" timestamp(6) NOT NULL,
+            CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE
+        ) WITH (OIDS= FALSE);
+
+        CREATE INDEX IF NOT EXISTS "IDX_session_expire" ON "session" ("expire");
+    `);
 }
 
 main().catch((error) => {
-    console.error(error);
-    process.exit(1);
+    panic(error);
 });
