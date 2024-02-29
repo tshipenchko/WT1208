0411086d8914c93f444175044b08629200be5dee
Łukasz Tshipenchko <dev@zxc.sx>
2024-03-01 01:54:18 +0500

    Added a random user API

--- assignment04/routes/ext.js
+++ assignment04/routes/ext.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const RANDOM_WORDS_URL = "https://random-word.ryanrk.com/api/en/word/random/10";
 const RANDOM_IMAGE_URL = "https://picsum.photos/600/200";
+const RANDOM_USERS_URL = "https://fakestoreapi.com/users";
 
 router.get("/random/sentence", (req, res) => {
     fetch(RANDOM_WORDS_URL)
@@ -31,5 +32,35 @@ router.get("/random/image", async (req, res) => {
         res.status(500).send("Error fetching image");
     }
 });
+
+router.get("/random/user", async (req, res) => {
+    try {
+        const userResponse = await fetch(RANDOM_USERS_URL);
+
+        if (!userResponse.ok) {
+            console.error(`Failed to fetch user: ${userResponse.statusText}`);
+            res.send(500).json({ msg: "Failed to fetch user" });
+            return;
+        }
+
+        const users = await userResponse.json();
+        const user = users[Math.floor(Math.random() * users.length)];
+
+        res.json({
+            firstName: capitalizeFirstLetter(user.name.firstname),
+            lastName: capitalizeFirstLetter(user.name.lastname),
+            email: user.email,
+            country: "USA",
+            gender: Math.random() < 0.5 ? "Male" : "Female",
+        })
+    } catch (error) {
+        console.error("Error fetching user:", error);
+        res.status(500).send("Error fetching user");
+    }
+});
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 module.exports = router;
