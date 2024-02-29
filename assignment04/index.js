0ff007486c7d25654bc6472b7f893854fe304f3e
Łukasz Tshipenchko <dev@zxc.sx>
2024-03-01 01:41:48 +0500

    Added logging

--- assignment04/index.js
+++ assignment04/index.js
@@ -4,11 +4,13 @@ const express = require("express");
 const session = require("express-session");
 const formData = require("express-form-data");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 const { requireEnv, requireEnvInt } = require("./utils");
 const { useRouter } = require("./routes");
 
 const app = express();
 
+app.use(morgan("combined"));
 app.use(express.static("public"));
 app.use("/uploads", express.static("uploads"));
 app.use(express.json());
